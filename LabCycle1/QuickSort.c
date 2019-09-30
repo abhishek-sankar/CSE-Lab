535a636be21370f83339c0a07939f7e201eb6d82
mssreerag <sreeragms@cet.ac.in>
2019-10-01 00:52:16 +0530

    vet\rthe

--- LabCycle1/QuickSort.c
+++ LabCycle1/QuickSort.c
@@ -61,12 +61,3 @@ void printArray(int arr[], int size)
 } 
   
 // Driver program to test above functions 
-int main() 
-{ 
-    int arr[] = {10, 7, 8, 9, 1, 5}; 
-    int n = sizeof(arr)/sizeof(arr[0]); 
-    quickSort(arr, 0, n-1); 
-    printf("The Sorted array: n"); 
-    printArray(arr, n); 
-    return 0; 
-} 
